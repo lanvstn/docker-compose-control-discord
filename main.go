ca023790a95c981d6a0612165ae66a66d300b123
Lander Visterin <34447895+lanvstn@users.noreply.github.com>
2021-03-07 14:48:49 +0100

    Ability to send events and keep state based on container logs (#1)

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"flag"
 	"fmt"
@@ -8,12 +9,16 @@ import (
 	"os"
 	"os/exec"
 	"os/signal"
+	"path"
+	"runtime/debug"
+	"strconv"
 	"strings"
 	"sync/atomic"
 	"syscall"
 	"time"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/docker/docker/client"
 )
 
 const (
@@ -24,15 +29,30 @@ const (
 	lockDurationStatus    = 5 * time.Second
 )
 
-var testMode bool
+var (
+	testMode          bool
+	targetProjectName string
+	targetService     string
+)
+
+// TODO: move these vars to somewhere not global
 
 var (
-	cancelStopCh chan struct{}
-	lock         uint32
+	cancelStopCh        chan struct{}
+	lock                uint32
+	lastExit            time.Time
+	as                  *appState
+	dcli                *client.Client
+	guildCreateReceived chan struct{}
 )
 
 func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
 	flag.BoolVar(&testMode, "test", false, "enable test mode")
+	flag.StringVar(&targetProjectName, "target-project-name", "", "docker compose project name of the target project, default: name of cwd")
+	flag.StringVar(&targetService, "target-service", "", "service in the docker compose project to use for the appState")
 	flag.Parse()
 
 	token := os.Getenv("TOKEN")
@@ -40,17 +60,42 @@ func main() {
 		log.Fatal("no token")
 	}
 
+	if targetProjectName == "" {
+		cwd, err := os.Getwd()
+		if err != nil {
+			log.Fatal(err)
+		}
+		targetProjectName = path.Base(cwd)
+	}
+
+	var err error
+	lastExit, err = readExitTime()
+	if err != nil {
+		log.Fatalf("failed to read exit time: %v", err)
+	}
+
+	dcli, err = client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		log.Fatalf("failed to init dockerclient: %v", err)
+	}
+
+	as = newAppState(dcli, valheimStateHandlerFuncs)
+	events := make(chan string)
+	go as.handleMessages(ctx, events)
+
+	guildCreateReceived = make(chan struct{}) // TODO proper guild state tracking?
+
 	dg, err := discordgo.New("Bot " + token)
 	if err != nil {
 		log.Fatalf("failed creating bot: %s", err.Error())
 	}
 
 	dg.AddHandler(messageCreate)
+	dg.AddHandler(guildCreate)
 
 	dg.State.TrackRoles = true
 	dg.StateEnabled = true
-
-	dg.Identify.Intents = discordgo.IntentsGuildMessages
+	dg.Identify.Intents = discordgo.IntentsGuildMessages | discordgo.IntentsGuilds
 
 	err = dg.Open()
 	if err != nil {
@@ -59,11 +104,52 @@ func main() {
 	}
 	defer dg.Close()
 
-	// Wait here until CTRL-C or other term signal is received.
-	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	go func() {
+		<-guildCreateReceived
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case msg := <-events:
+				broadcast(dg, msg)
+			}
+		}
+	}()
+
+	fmt.Println("docker-compose-control bot is now running.")
+
+	_ = monitorTarget() // try to resume monitoring
+
+	defer func() {
+		err := writeExitTime()
+		if err != nil {
+			log.Printf("failed to write exit time: %v", err)
+		}
+	}()
+
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM)
 	<-sc
+}
+
+func broadcast(s *discordgo.Session, msg string) {
+	log.Printf("broadcast: %v\n", msg)
+	for _, g := range s.State.Guilds {
+		for _, c := range g.Channels {
+			if c.Name == "general" { // TODO support choosing broadcast channel
+				_, err := s.ChannelMessageSend(c.ID, msg)
+				if err != nil {
+					log.Printf("broadcast failed to guild %v: %v\n", g.ID, err)
+				}
+				continue
+			}
+		}
+	}
+}
+
+func guildCreate(s *discordgo.Session, g *discordgo.GuildCreate) {
+	log.Printf("guild create: %v (%v)", g.Guild.ID, g.Guild.Name)
+	guildCreateReceived <- struct{}{}
 }
 
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
@@ -78,12 +164,13 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	defer func() {
 		if err := recover(); err != nil {
 			log.Printf("PANIC while processing message: %s", err)
+			debug.PrintStack()
 		}
 	}()
 
 	if !userAllowed(s, m, requiredRole) {
 		log.Printf("user %v does not have required role %s in %v", m.Author.String(), requiredRole, m.Member.Roles)
-		respond(s, m, "permission denied")
+		respond(s, m, "Permission denied")
 		return
 	}
 
@@ -98,28 +185,34 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	switch cmd[0] {
 	case "start":
 		if !lockDuration(lockDurationStartStop) {
-			respond(s, m, "calm the fuck down")
+			respond(s, m, "Calm the fuck down")
 			return
 		}
 
 		if err := startServer(); err != nil {
-			respond(s, m, "failed to start server: %s", err.Error())
+			respond(s, m, "Failed to start server: %s", err.Error())
 			return
 		}
-		respond(s, m, "server has been started")
+
+		if err := monitorTarget(); err != nil {
+			respond(s, m, fmt.Sprintf("Server has been started but: %v", err))
+			return
+		}
+
+		respond(s, m, "Server has been started")
 		return
 	case "stop":
 		var delay time.Duration
 
 		if len(cmd) == 2 {
 			if !userAllowed(s, m, adminRole) {
-				respond(s, m, "permission denied, use stop command without parameters")
+				respond(s, m, "Permission denied, use stop command without parameters")
 				return
 			}
 			if cmd[1] == "now" {
 				delay = 10 * time.Second
 			} else {
-				respond(s, m, "invalid parameter, options: now")
+				respond(s, m, "Invalid parameter, options: now")
 				return
 			}
 		} else {
@@ -127,33 +220,37 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		}
 
 		if !lockDuration(lockDurationStartStop) {
-			respond(s, m, "calm the fuck down")
+			respond(s, m, "Calm the fuck down")
 			return
 		}
 
 		if err := delayedStopServer(delay, func(msg string) { respond(s, m, msg) }); err != nil {
-			respond(s, m, "failed to intiate server stop: %s", err.Error())
+			respond(s, m, "Failed to intiate server stop: %s", err.Error())
 			return
 		}
-		respond(s, m, "stopping server in %s", delay.String())
+		respond(s, m, "Stopping server in %s", delay.String())
 		return
 	case "cancel":
 		if err := cancelStop(); err != nil {
-			respond(s, m, "failed to cancel stop: %s", err.Error())
+			respond(s, m, "Failed to cancel stop: %s", err.Error())
 			return
 		}
-		respond(s, m, "canceled stop")
+		respond(s, m, "Canceled stop")
 		return
 	case "status":
 		if !lockDuration(lockDurationStatus) {
-			respond(s, m, "calm the fuck down")
+			respond(s, m, "Calm the fuck down")
 			return
 		}
 
-		respond(s, m, getStatus())
+		if getStatus() {
+			respond(s, m, "Server is currently up.")
+		} else {
+			respond(s, m, "Server is currently down.")
+		}
 		return
 	default:
-		respond(s, m, "unknown command")
+		respond(s, m, "Unknown command")
 		return
 	}
 }
@@ -217,14 +314,6 @@ func updateRoleState(s *discordgo.Session, guildID string) error {
 	return nil
 }
 
-func startServer() error {
-	return runCommand("docker-compose", "up", "-d")
-}
-
-func stopServer() error {
-	return runCommand("docker-compose", "down")
-}
-
 func delayedStopServer(delay time.Duration, onStopped func(msg string)) error {
 	if cancelStopCh != nil {
 		return errors.New("a server stop is already pending")
@@ -258,23 +347,6 @@ func cancelStop() error {
 	return nil
 }
 
-func getStatus() string {
-	if testMode {
-		return "test mode, status unknown"
-	}
-
-	out, err := exec.Command("docker-compose", "top").CombinedOutput()
-	if err != nil {
-		return fmt.Sprintf("failed to get status: %s", err.Error())
-	}
-
-	if strings.TrimSpace(string(out)) == "" {
-		return "server currently down"
-	}
-
-	return "server currently up"
-}
-
 // lockDuration locks in the background for duration if possible
 //  and returns true if able to lock
 func lockDuration(d time.Duration) bool {
@@ -302,4 +374,37 @@ func runCommand(name string, args ...string) error {
 	}
 
 	return nil
+}
+
+func monitorTarget() error {
+	c, err := appContainer(context.TODO(), dcli, targetProjectName, targetService)
+	if err != nil {
+		return fmt.Errorf("could not find the container so monitoring will fail: %w", err)
+	}
+
+	log.Printf("found appContainer %v\n", c.ID)
+
+	if err := as.monitorContainer(context.Background(), c.ID); err != nil {
+		return fmt.Errorf("could not start monitoring the container after finding it: %w", err)
+	}
+
+	return nil
+}
+
+func writeExitTime() error {
+	return os.WriteFile("stoptime", []byte(strconv.Itoa(int(time.Now().Unix()))), 0o600)
+}
+
+func readExitTime() (time.Time, error) {
+	content, err := os.ReadFile("stoptime")
+	if os.IsNotExist(err) {
+		return time.Time{}, nil
+	}
+
+	i, err := strconv.Atoi(string(content))
+	if err != nil {
+		return time.Time{}, err
+	}
+
+	return time.Unix(int64(i), 0), nil
 }
